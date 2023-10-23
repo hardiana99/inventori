c1e5a7dc2e23a6adffc99d6069aab98c708f72de
dian <diianhs0499@gmail.com>
2023-10-23 19:36:12 +0900

    Co-authored-by: Ajenkris Kungkung <kristt26@gmail.com>

---
+++ app/Controllers/Barang.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Barang extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function tambah(){
+        
+    }
+
+    public function ubah(){
+
+    }
+
+    public function hapus(){
+
+    }
+}
